5f26d72bfbb20aa3123a52dd93e32438d6b94bdf
Oleg Shevchuk <papalom31@gmail.com>
2017-10-01 22:35:28 +0300

    abstract

--- src/test/java/com/softserve/if078/tmwSpring/services/UserPopulatorTest.java
+++ src/test/java/com/softserve/if078/tmwSpring/services/UserPopulatorTest.java
@@ -10,19 +10,21 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.sql.SQLException;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RunWith(SpringRunner.class)
 @ComponentScan(basePackages = "com.softserve.if078.tmwSpring")
-@SpringBootTest(classes = {H2DbConfig.class })
+@SpringBootTest(classes = {H2DbConfig.class})
 @EnableConfigurationProperties
 public class UserPopulatorTest {
+  @Autowired
+  UserPopulator populator;
 
-    @Test
-    public void checkIdAutoIncremented () throws SQLException {
-        UserPopulator populator = new UserPopulator();
-        User user = populator.createDefaultUser();
-        Assert.assertNotEquals(-1, user.getId());
-    }
+  @Test
+  public void checkIdAutoIncremented() throws SQLException {
+    // UserPopulator populator = new UserPopulator();
+    User user = populator.createDefaultUser();
+    Assert.assertNotEquals(-1, user.getId());
+  }
 }
