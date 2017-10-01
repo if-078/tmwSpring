a0305d68a98ca7bb5c76b1a6607290fb89ab36ac
Світлана Овчарук <svitlana.ovcharuk@gmail.com>
2017-10-01 17:05:09 +0300

    add classes for Task

---
+++ src/main/java/com/softserve/if078/tmwSpring/entities/Priority.java
@@ -0,0 +1,26 @@
+package com.softserve.if078.tmwSpring.entities;
+
+public class Priority {
+    private int id;
+    private String name;
+
+    public Priority() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/softserve/if078/tmwSpring/services/TaskService.java
@@ -0,0 +1,96 @@
+package com.softserve.if078.tmwSpring.services;
+
+import com.softserve.if078.tmwSpring.dao.TaskDaoInterface;
+import com.softserve.if078.tmwSpring.entities.Comment;
+import com.softserve.if078.tmwSpring.entities.Tag;
+import com.softserve.if078.tmwSpring.entities.Task;
+import com.softserve.if078.tmwSpring.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TaskService implements TaskServiceInterface {
+
+    @Autowired
+    TaskDaoInterface taskDao;
+
+    @Override
+    public List<Task> getAll() throws SQLException {
+        return taskDao.getAll();
+    }
+
+    @Override
+    public Task get(int id) throws SQLException {
+        return taskDao.findOne(id);
+    }
+
+    @Override
+    public void update(Task task) throws SQLException {
+        taskDao.update(task);
+    }
+
+    @Override
+    public void delete(int id) throws SQLException {
+        taskDao.delete(id);
+    }
+
+    @Override
+    public void create(Task task) throws SQLException {
+        taskDao.create(task);
+    }
+
+    @Override
+    public ArrayList<Task> getTaskByStatus(int statusId) {
+        return taskDao.getTaskByStatus(statusId);
+    }
+
+    @Override
+    public ArrayList<Task> getTaskByPriority(int priorityId) {
+        return taskDao.getTaskByPriority(priorityId);
+    }
+
+    @Override
+    public ArrayList<Task> getTasksCreatedByUser(int userId) {
+        return taskDao.getTasksCreatedByUser(userId);
+    }
+
+    @Override
+    public ArrayList<Task> getTasksAssignToUser(int userId) {
+        return taskDao.getTasksAssignToUser(userId);
+    }
+
+    @Override
+    public ArrayList<Task> getTasksForToday() {
+        return taskDao.getTasksForToday();
+    }
+
+    @Override
+    public ArrayList<Task> getTasksByTag(int tagId) {
+        return taskDao.getTasksByTag(tagId);
+    }
+
+    @Override
+    public  ArrayList<Task> treeOfTasks(int taskId) {
+        return taskDao.treeOfTasks(taskId);
+    }
+
+    @Override
+    public User getAuthorOfTask(int taskId) {
+        return taskDao.getAuthorOfTask(taskId);
+    }
+
+    @Override
+    public ArrayList<Tag> getTagsOfTask(int taskId) {
+        return taskDao.getTagsOfTask(taskId);
+    }
+
+    @Override
+    public ArrayList<Comment> getCommentsOfTask(int taskId) {
+        return taskDao.getCommentsOfTask(taskId);
+    }
+
+}
